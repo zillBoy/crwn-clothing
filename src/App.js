faa65cfe1b74744a3d92d184269ce5e4eb24a2a0
Muhammad Usman <prousman741@gmail.com>
2021-11-30 22:42:00 +0500

    Navigation is set up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 import './styles/App.css'
 import Homepage from './pages/Homepage.component'
 
 const App = () => {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+      </Routes>
     </div>
   )
 }
